306a36e8522101e490b2777bc1bd3d6c053da100
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-02-05 15:27:47 -0300

    Fixes Header Component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header';
+import { Header } from './components/Header/Header';
 import { Home } from './Pages/Home';
 import { Contact } from './Pages/Contact';
 import { Product } from './Pages/Product';
